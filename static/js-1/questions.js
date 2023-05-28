a837b09b37236bbaa584c4313692500bfadacfc4
prathik <20aiml0232@vvce.ac.in>
2023-05-28 08:15:39 +0530

    Initial commit

---
+++ static/js-1/questions.js
@@ -0,0 +1,46 @@
+const questions = [
+  {
+    numb: 1,
+    question: "which method sends html form data to server?",
+    answer: "post",
+    options: [
+      "get",
+      "head",
+      "post",
+      "put",
+    ]
+  },
+  {
+    numb: 1,
+    question: "flask is used for?",
+    answer: "web development",
+    options: [
+      "web development",
+      "mobile development",
+      "database development",
+      "cloud development",
+    ]
+  },
+  {
+    numb: 1,
+    question: "which of the following are types of unicode character string types in SQL?",
+    answer: "a&b",
+    options: [
+      "nchar",
+      "ntext",
+      "a&b",
+      "none",
+    ]
+  },
+  {
+    numb: 1,
+    question: "which of the following are some common RDBMS in use?",
+    answer: "all of the above",
+    options: [
+      "oracle",
+      "mySql",
+      "heidiSql",
+      "all of the above",
+    ]
+  },
+];
